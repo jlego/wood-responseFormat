509dd9aced9e66734cc3201c66815c98aaf3fd3a
jlego <273734893@qq.com>
2018-11-24 19:07:53 +0800

    no message

--- index.js
+++ index.js
@@ -4,9 +4,30 @@
  * by jlego on 2018-11-17
  */
  const zlib = require('zlib');
- const Util = require('./src/util');
 
 module.exports = (app = {}, config = {}) => {
+  let respData = function(data, reqData){
+    let status = 0,
+      msg = '';
+    if (!data && data !== false) data = WOOD.config.errorCode.error_nodata;
+    if (data.path && data.message && data.kind) { //返回错误
+      status = WOOD.config.errorCode.error_wrongdata.code;
+      msg = WOOD.config.errorCode.error_wrongdata.msg;
+    } else if (data.name == 'ValidationError') {
+      status = WOOD.config.errorCode.error_validation.code;
+      msg = WOOD.config.errorCode.error_validation.msg;
+    } else {
+      status = !data.code ? WOOD.config.errorCode.success.code : data.code;
+      msg = !data.msg ? WOOD.config.errorCode.success.msg : data.msg;
+    }
+    return {
+      // seqno: reqData.seqno,
+      cmd: reqData.cmd,
+      status,
+      msg,
+      data: !data.code ? data : {}
+    };
+  };
   app.responseFormat = function(req, res, next) {
     res.print = function(data){
       let body = {};
@@ -15,7 +36,7 @@ module.exports = (app = {}, config = {}) => {
       }else{
         body = req.body;
       }
-      let result = Util.respData(data.err ? Util.error(data.err) : (data.hasOwnProperty('data') ? data.data : data), body);
+      let result = respData(data.err ? WOOD.error(data.err) : (data.hasOwnProperty('data') ? data.data : data), body);
       let resultStr = JSON.stringify(result);
       // 压缩结果
       res.statusCode = 200;
