1d865c8eb2dfca1f886de8e8e0681a0d52dc3c5f
jlego <273734893@qq.com>
2018-11-25 12:05:41 +0800

    no message

--- index.js
+++ index.js
@@ -4,21 +4,22 @@
  * by jlego on 2018-11-17
  */
  const zlib = require('zlib');
+ const { config } = WOOD;
 
 module.exports = (app = {}, config = {}) => {
   let respData = function(data, reqData){
     let status = 0,
       msg = '';
-    if (!data && data !== false) data = WOOD.config.errorCode.error_nodata;
+    if (!data && data !== false) data = config.errorCode.error_nodata;
     if (data.path && data.message && data.kind) { //返回错误
-      status = WOOD.config.errorCode.error_wrongdata.code;
-      msg = WOOD.config.errorCode.error_wrongdata.msg;
+      status = config.errorCode.error_wrongdata.code;
+      msg = config.errorCode.error_wrongdata.msg;
     } else if (data.name == 'ValidationError') {
-      status = WOOD.config.errorCode.error_validation.code;
-      msg = WOOD.config.errorCode.error_validation.msg;
+      status = config.errorCode.error_validation.code;
+      msg = config.errorCode.error_validation.msg;
     } else {
-      status = !data.code ? WOOD.config.errorCode.success.code : data.code;
-      msg = !data.msg ? WOOD.config.errorCode.success.msg : data.msg;
+      status = !data.code ? config.errorCode.success.code : data.code;
+      msg = !data.msg ? config.errorCode.success.msg : data.msg;
     }
     return {
       // seqno: reqData.seqno,
@@ -28,6 +29,7 @@ module.exports = (app = {}, config = {}) => {
       data: !data.code ? data : {}
     };
   };
+
   app.responseFormat = function(req, res, next) {
     res.print = function(data){
       let body = {};
@@ -36,7 +38,7 @@ module.exports = (app = {}, config = {}) => {
       }else{
         body = req.body;
       }
-      let result = respData(data.err ? WOOD.error(data.err) : (data.hasOwnProperty('data') ? data.data : data), body);
+      let result = respData(data.err ? error(data.err) : (data.hasOwnProperty('data') ? data.data : data), body);
       let resultStr = JSON.stringify(result);
       // 压缩结果
       res.statusCode = 200;
